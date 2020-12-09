239d43e949fa422133c105b7cf31d366204fe2ce
Hiero Salim <salimhiero@gmail.com>
2020-12-08 22:30:52 -0300

    API Login Funcionando

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.get('/', (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({error : error})}
         conn.query(
-            'SELECT * FROM user;',
+            'SELECT * FROM User;',
             (error, resultado, fields) => {
                 if(error) { return res.status(500).send({error : error})}
                 return res.status(200).send({response: resultado})
@@ -21,7 +21,7 @@ router.get('/:CPF', (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({error : error})}
         conn.query(
-            'SELECT * FROM user WHERE CPF = ?;',
+            'SELECT * FROM User WHERE CPF = ?;',
             [req.params.CPF],
             (error, resultado, fields) => {
                 if(error) { return res.status(500).send({error : error})}
@@ -41,13 +41,40 @@ router.post('/', (req, res, next) => {
             (error, resultado, field) =>{
                 conn.release();
                 if(error) { return res.status(500).send({error : error})}
-
                 res.status(201).send({
                     menssagem: 'Cadastrado com sucesso!',
                     CPF: resultado.InsertCPF
                 });
             }
         )
+    });
+});
+
+router.post('/Authentication', (req, res, next) =>{
+    mysql.getConnection((error, conn) =>{
+        if (error) { return res.status(500).send({error : error})} 
+            conn.query(
+                'SELECT CPF, user, pass, typeUser from User WHERE user = ? and pass = ?',
+                [req.body.user, req.body.pass],
+                (error ,resultado, field) =>{
+                    conn.release();
+                    if(error) { return res.status(500).send({error : error})}
+                    else if (resultado.length > 0){
+                        if (resultado[0].user == req.body.user && resultado[0].pass == req.body.pass) {
+                            res.status(201).send({             
+                                user : resultado[0].user,
+                                pass : resultado[0].pass,       
+                                typeUser: resultado[0].typeUser,
+                                CPF: resultado[0].CPF
+                            });       
+                        }
+                    }else{
+                        res.status(404).send({                    
+                            menssagem: 'Usuário ou senha incorretos'
+                        });
+                    }
+                }
+            )
     });
 });
 
@@ -90,7 +117,7 @@ router.delete('/', (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({error : error})}
         conn.query(
-            `DElETE FROM user WHERE CPF = ?`, [req.body.CPF],
+            `DElETE FROM User WHERE CPF = ?`, [req.body.CPF],
             (error, resultado, field) =>{
                 conn.release();
                 if(error) { return res.status(500).send({error : error})}
